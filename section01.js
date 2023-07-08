6082ace068477268bb776a8819e2da94b003b7aa
Leejihi <ttoksunyi@naver.com>
2023-07-08 13:58:26 +0900

    index.html 등록, section01 영역 영상확대, 영역도착시 밑줄, 호버 이벤트 추가를 위해 section01.js, section01-style.css 수정! images 폴더에 og-img.jpg 파일 생성하여 오픈그라피 연결

--- section01.js
+++ section01.js
@@ -1,11 +1,31 @@
-// 타이틀 이미지 움직임
+// jh 스크롤은 내리면 영상이 커지는 이벤트
+
+$(window).scroll(function () {
+  const scroll = $(window).scrollTop();
+  $("#mov01").css({
+    width: 100 + scroll / 18 + "%",
+  });
+});
+
+// jh 영역 도착시, 밑줄 발생
+$(window).on("scroll", () => {
+  const underHt = $("h3").offset().top - 500;
+
+  if (scrollY > underHt) {
+    $("h3").addClass("under");
+  } else {
+    $("h3").removeClass("under");
+  }
+});
+
+// jh 섹션영역1에서 마우스 움직임 발생시 banner-box 이미지 움직임
 const movingImg = (evt) => {
   let x = evt.clientX;
   let y = evt.clientY;
 
-  $('#banner-title').css({ left: 20 - x / 60, bottom: 20 - y / 30 });
+  $("#banner-title").css({ left: 20 - x / 60, bottom: 20 - y / 30 });
 
   requestAnimationFrame(movingImg);
 };
 
-$('.banner-wrapper').on('mousemove', movingImg);
+$(".banner-wrapper").on("mousemove", movingImg);
