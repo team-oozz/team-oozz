82f30040ae1a79871334ebf4b1d7afdc5d091ebb
youngsoAN <whirosa@naver.com>
2023-07-09 15:03:31 +0900

    sy 파일추가

--- map/map.js
+++ map/map.js
@@ -38,23 +38,3 @@ $(".section1 img").each(function () {
     });
   });
 });
-
-// 인터넷 긁어온거
-var header = document.querySelector("header");
-var headerMoving = function (direction) {
-  if (direction === "up") {
-    header.className = "";
-  } else if (direction === "down") {
-    header.className = "scrollDown";
-  }
-};
-var prevScrollTop = 0;
-document.addEventListener("scroll", function () {
-  var nextScrollTop = window.pageYOffset || 0; // pageYOffset -> IE 8 이하 빼고 다 됨.
-  if (nextScrollTop > prevScrollTop) {
-    headerMoving("down");
-  } else if (nextScrollTop < prevScrollTop) {
-    headerMoving("up");
-  }
-  prevScrollTop = nextScrollTop;
-});
