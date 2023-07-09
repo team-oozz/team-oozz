e1cba00a98168f75f3e147aa07bc7bc9f4a015e4
youngsoAN <whirosa@naver.com>
2023-07-09 13:53:55 +0900

    lineup scroll event
    라인업 스크롤 내리면 헤더 숨겨지고 올리면 나타남

--- LineUp/LineUp.js
+++ LineUp/LineUp.js
@@ -38,3 +38,22 @@ $(".upto").click(function () {
   $("html, body").animate({ scrollTop: 0 }, 400);
   return false;
 });
+// 인터넷 긁어온거
+var header = document.querySelector("header");
+var headerMoving = function (direction) {
+  if (direction === "up") {
+    header.className = "";
+  } else if (direction === "down") {
+    header.className = "scrollDown";
+  }
+};
+var prevScrollTop = 0;
+document.addEventListener("scroll", function () {
+  var nextScrollTop = window.pageYOffset || 0; // pageYOffset -> IE 8 이하 빼고 다 됨.
+  if (nextScrollTop > prevScrollTop) {
+    headerMoving("down");
+  } else if (nextScrollTop < prevScrollTop) {
+    headerMoving("up");
+  }
+  prevScrollTop = nextScrollTop;
+});
